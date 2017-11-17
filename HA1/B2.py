62fb3921a152d4d272688fa8019adacb1f4af842
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2017-11-17 17:06:46 +0100

    added example values from test quiz, as well as sort-of-pseudocode to sketch program, plus explanatory comments for parameters etc

--- HA1/B2.py
+++ HA1/B2.py
@@ -9,6 +9,34 @@ def confidence_interval(x_hat, sigma, n, gamma=3.66):
     
     return "nada"
 
-test = [0, 1, 2, 3]
+if __name__ == '__main__':
+    u = 16 # number of bits used for ID of bin
+    k = 2 # number of collisions needed to make a coin
+    c = 1 # target, the number of coins to create. Can be one of [1, 100, 10000]
 
-print(numpy.mean(test))
+    coin_list = [] # a list w/ created coins
+    tossed_balls = {} # a dictionary where keys are computed hashes and the values the x:es that result in the key hash value
+
+#print(numpy.mean(test))
+
+'''
+    # a sort-of pseudocode sketch
+
+    while len(coinsList) < c
+        randomize r
+        calc h(r)
+        dict[h(r)].append(r) # plus an empty-key-check
+        if len(dict[h(r)]) == k:
+            coinsList.append(h(r))
+    
+    # example values output from test quiz
+    c = 10000
+    u = 20
+    k = 7
+    confidence interval width = 4783
+
+    c = 1
+    u = 16
+    k = 2
+    confidence interval width = 22
+'''
