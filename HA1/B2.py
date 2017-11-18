539b01e92fd0ec9f53b56acb3d224b64bd788d2b
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2017-11-18 12:35:58 +0100

    skeleton for functions, plus examples of the statistic methods to use

--- HA1/B2.py
+++ HA1/B2.py
@@ -1,23 +1,34 @@
-# TODO: write confidence_interval function
-# TODO: write generate_coins function
-
-import scipy.stats as stats
-import numpy
+import math
+import statistics
 
 def confidence_interval(x_hat, sigma, n, gamma=3.66):
     ci = 0
     
     return "nada"
 
+def generate_coins(u, k, c):
+
+    return "coins!"
+
 if __name__ == '__main__':
     u = 16 # number of bits used for ID of bin
     k = 2 # number of collisions needed to make a coin
     c = 1 # target, the number of coins to create. Can be one of [1, 100, 10000]
+    ci_width = 22
+    ci = math.inf
 
     coin_list = [] # a list w/ created coins
     tossed_balls = {} # a dictionary where keys are computed hashes and the values the x:es that result in the key hash value
 
-#print(numpy.mean(test))
+    while ci > ci_width:
+        #generate_coins(u, k, c)
+        #ci = confidence_interval(blablabla)
+        pass
+    
+    test = [1, 2, 3, 4, 5]
+    print(statistics.mean(test))
+    print(statistics.stdev(test))
+    print(statistics.variance(test))
 
 '''
     # a sort-of pseudocode sketch
