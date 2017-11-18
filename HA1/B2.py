fbf801cbc2979c2fca6d74ae224d254d62c7acae
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2017-11-18 14:55:19 +0100

    Filled in functions, program flow works. Now: get details right

--- HA1/B2.py
+++ HA1/B2.py
@@ -1,14 +1,47 @@
 import math
 import statistics
+import hashlib
+import random
 
-def confidence_interval(x_hat, sigma, n, gamma=3.66):
-    ci = 0
-    
-    return "nada"
+def confidence_interval(iterations_list):
+    #x_hat, sigma, n, gamma=3.66
+    n_squared = math.sqrt(len(iterations_list))
+    s = statistics.stdev(iterations_list)
+    gamma = 3.66
+    x_hat = statistics.mean(iterations_list)  
+    ci =  gamma*s/n_squared
+    print(ci)
+    return ci
 
 def generate_coins(u, k, c):
+    coin_list = [] # a list w/ created coins
+    # a dictionary where keys are computed hashes and the values 
+    # the x:es that result in the key hash value
+    tossed_balls = {} 
+    iterations = 0
 
-    return "coins!"
+    while len(coin_list) < c:
+        x = random.randrange(255).to_bytes(2, byteorder="big")
+        h = hashlib.md5(x).digest()
+        mask = 0b1
+        for i in range(u):
+            mask = mask | (0b1 << i)
+        #print(len(bin(int.from_bytes(h, byteorder="big"))))
+        #print(bin(int.from_bytes(h, byteorder="big")))
+        h = int.from_bytes(h, byteorder="big") & mask
+        #print(bin(mask))
+        #print(bin(h))
+        #print(h)
+
+        if h not in tossed_balls.keys():
+            tossed_balls[h] = [x]
+        else:
+            tossed_balls[h].append(x)
+            if len(tossed_balls[h]) == k:
+                coin_list.append(h)
+        iterations += 1
+
+    return iterations
 
 if __name__ == '__main__':
     u = 16 # number of bits used for ID of bin
@@ -16,19 +49,27 @@ if __name__ == '__main__':
     c = 1 # target, the number of coins to create. Can be one of [1, 100, 10000]
     ci_width = 22
     ci = math.inf
-
-    coin_list = [] # a list w/ created coins
-    tossed_balls = {} # a dictionary where keys are computed hashes and the values the x:es that result in the key hash value
+    iterations_list = []
 
     while ci > ci_width:
-        #generate_coins(u, k, c)
-        #ci = confidence_interval(blablabla)
+        iterations_list.append(generate_coins(u, k, c))
+        if len(iterations_list) > 1:
+            ci = confidence_interval(iterations_list)
         pass
     
-    test = [1, 2, 3, 4, 5]
-    print(statistics.mean(test))
-    print(statistics.stdev(test))
-    print(statistics.variance(test))
+    print(iterations_list)
+    print("Mean of iterations: ", end="")
+    print(statistics.mean(iterations_list))
+    print("Standard deviation of iterations: ", end="")
+    print(statistics.stdev(iterations_list))
+    print("Median of iterations: ", end="")
+    print(statistics.median(iterations_list))
+
+    
+    # test = [1, 2, 3, 4, 5]
+    # print(statistics.mean(test))
+    # print(statistics.stdev(test))
+    # print(statistics.variance(test))
 
 '''
     # a sort-of pseudocode sketch
