a5c9c86848df0ddbfdbea9e06942ff39ba839f37
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2017-11-18 18:05:23 +0100

    added timekeeping, facilitation of switching between test parameters and a more correct generation of x:es for hashing

--- HA1/B2.py
+++ HA1/B2.py
@@ -2,9 +2,9 @@ import math
 import statistics
 import hashlib
 import random
+import time
 
 def confidence_interval(iterations_list):
-    #x_hat, sigma, n, gamma=3.66
     n_squared = math.sqrt(len(iterations_list))
     s = statistics.stdev(iterations_list)
     gamma = 3.66
@@ -21,17 +21,15 @@ def generate_coins(u, k, c):
     iterations = 0
 
     while len(coin_list) < c:
-        x = random.randrange(255).to_bytes(2, byteorder="big")
+        # x = random.randrange(255).to_bytes(2, byteorder="big")
+        # x = random.getrandbits(u).to_bytes(3, byteorder="big")
+        x = random.getrandbits(4*8)
+        x = x.to_bytes(4, byteorder="big")
         h = hashlib.md5(x).digest()
         mask = 0b1
         for i in range(u):
             mask = mask | (0b1 << i)
-        #print(len(bin(int.from_bytes(h, byteorder="big"))))
-        #print(bin(int.from_bytes(h, byteorder="big")))
         h = int.from_bytes(h, byteorder="big") & mask
-        #print(bin(mask))
-        #print(bin(h))
-        #print(h)
 
         if h not in tossed_balls.keys():
             tossed_balls[h] = [x]
@@ -47,49 +45,46 @@ if __name__ == '__main__':
     u = 16 # number of bits used for ID of bin
     k = 2 # number of collisions needed to make a coin
     c = 1 # target, the number of coins to create. Can be one of [1, 100, 10000]
-    ci_width = 22
+    ci_width = 22 # the target width of confidence interval
+
+    # a dictionary to facilitate switching between test values;
+    # the parameters in the tuples are the following:
+    # (u, k, c, ci_width)
+    parameters = {  1: (16, 2, 1, 22),
+                    2: (16, 2, 100, 24),
+                    3: (16, 2, 10000, 22),
+                    4: (20, 7, 1, 79671),
+                    5: (20, 7, 100, 15616),
+                    6: (20, 7, 10000, 4783)}
+    # choose combination of test parameters here:
+    combination = 4
+
     ci = math.inf
     iterations_list = []
 
-    while ci > ci_width:
-        iterations_list.append(generate_coins(u, k, c))
-        if len(iterations_list) > 1:
+    start_time = time.time()
+    
+    while ci > parameters[combination][3]:
+        iterations_list.append(generate_coins(parameters[combination][0], 
+                                parameters[combination][1], 
+                                parameters[combination][2]))
+        if len(iterations_list) > 10:
             ci = confidence_interval(iterations_list)
-        pass
+    
+    end_time = time.time() - start_time
     
     print(iterations_list)
+    print("\n")
     print("Mean of iterations: ", end="")
     print(statistics.mean(iterations_list))
     print("Standard deviation of iterations: ", end="")
     print(statistics.stdev(iterations_list))
     print("Median of iterations: ", end="")
     print(statistics.median(iterations_list))
-
+    print("Time for execution: ", end="")
+    print(end_time)
     
     # test = [1, 2, 3, 4, 5]
     # print(statistics.mean(test))
     # print(statistics.stdev(test))
     # print(statistics.variance(test))
-
-'''
-    # a sort-of pseudocode sketch
-
-    while len(coinsList) < c
-        randomize r
-        calc h(r)
-        dict[h(r)].append(r) # plus an empty-key-check
-        if len(dict[h(r)]) == k:
-            coinsList.append(h(r))
-    
-    # example values output from test quiz
-    c = 10000
-    u = 20
-    k = 7
-    confidence interval width = 4783
-
-    c = 1
-    u = 16
-    k = 2
-    confidence interval width = 22
-
-'''
