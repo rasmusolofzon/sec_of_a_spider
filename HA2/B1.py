b8927243344877592f4debc322c01cf5fc5f7001
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2017-11-27 11:52:14 +0100

    HA2: B1={plumbing with multiprocessing and so on works, also assignment instructions example where b=1 works}

--- HA2/B1.py
+++ HA2/B1.py
@@ -4,7 +4,14 @@ import multiprocessing as mp
 # from mp import Process, Queue
 
 def dinner(q, dinner_input):
-    q.put(dinner_input)
+    if dinner_input[6] == 1:
+        dinner_output = dinner_input[1] ^ dinner_input[2] ^ dinner_input[5]
+    else:
+        dinner_output = dinner_input[1] ^ dinner_input[2] ^ 0
+    
+    msg = dinner_output ^ dinner_input[3] ^ dinner_input[4]
+
+    q.put((dinner_input[0], dinner_output, msg))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A 16 bit Dining Cryptographers net.", epilog="Please specify all parameters (except -h, of course).")
@@ -29,23 +36,29 @@ if __name__ == "__main__":
     processes = []
 
     for i in range(16):
-        dinner_input = ( ((int.from_bytes(sa, byteorder="big") >> i) & mask), ((int.from_bytes(sb, byteorder="big") >> i) & mask), ((int.from_bytes(da, byteorder="big") >> i) & mask), ((int.from_bytes(db, byteorder="big") >> i) & mask), ((int.from_bytes(m, byteorder="big") >> i) & mask), args.b )
+        # (0,     1,  2,  3,  4,  5, 6)
+        # (index, SA, SB, DA, DB, M, b)
+        dinner_input = ( i, ((int.from_bytes(sa, byteorder="big") >> i) & mask), ((int.from_bytes(sb, byteorder="big") >> i) & mask), ((int.from_bytes(da, byteorder="big") >> i) & mask), ((int.from_bytes(db, byteorder="big") >> i) & mask), ((int.from_bytes(m, byteorder="big") >> i) & mask), args.b )
 
         p = mp.Process(target=dinner, args=(q, dinner_input,), name="p{0}".format(i))
         processes.append(p)
         p.start()
         print(dinner_input)
 
-    while True:
-        # print(mp.cpu_count())
-        1
-        if not all(proc.is_alive() for proc in processes):
-            break
-
     print()
 
-    while not q.empty():
-        print(q.get())
+    dinner_output = []
+    while len(dinner_output) < 16:
+        dinner_output.append(q.get())
+    
+    for output in dinner_output:
+        print(output)
+    
+    result = 0
+    for output in dinner_output:
+        result = result | (output[1] << output[0])
+    
+    print("\n\nProgram output is: {0}".format(hex(result)).upper())
 
     # print(args.sa, args.sb, args.da, args.db, args.m, args.b)
     # print(sa, sb, da, db, m)
@@ -64,7 +77,7 @@ if __name__ == "__main__":
                 if b == 1:
                     broadcast = SA ^ SB ^ M
                 elif b == 0:
-                    output = SA ^ SB ^ 0
+                    broadcast = SA ^ SB ^ 0
                     randomise if Alice, Bob or None sends msg
                         do this..
                 msg = broadcast ^ DA ^ DB
@@ -73,4 +86,8 @@ if __name__ == "__main__":
             ge alla returns, samlade
 
     python B1.py --sa 0C73 --sb 80C1 --da A2A9 --db 92F5 --m 9B57 --b 0 
+
+    27C2 0879 35F6 1A4D 27BC 1 0807
+    python B1.py --sa 27C2 --sb 0879 --da 35F6 --db 1A4D --m 27BC --b 1 
+        result 0807
     '''
