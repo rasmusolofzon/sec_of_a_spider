d52b229ccaa7c4d9f290686bf421b4a77bfa6a30
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2017-12-04 20:49:36 +0100

    HA3: B={added files, basically no work done yet}

---
+++ HA3/B1.py
@@ -0,0 +1,12 @@
+import random
+import hashlib
+
+k = random.getrandbits(16)
+v = random.getrandbits(1)
+
+for X in range(1,8):
+    ...
+    #binding
+    #concealing
+
+print("k = {0}, type {1}".format(k, type(k)))
