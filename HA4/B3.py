a8a7533c2eaae010c9d379afa04a581eda4a7844
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2017-12-12 17:02:57 +0100

    HA4: B3={a probably more correct version, bytearrays etc}

--- HA4/B3.py
+++ HA4/B3.py
@@ -3,8 +3,6 @@ import math
 import argparse
 import binascii
 
-h_len = 8 # length of hash output in octets
-
 def i2osp(x, x_len):
     """Convert a nonnegative integer to an octet string of a
     specified length. (Integer-to-Octet-String primitive)
@@ -14,18 +12,21 @@ def i2osp(x, x_len):
     xLen -- intended length of the resulting octet string
 
     Returns:
-    X -- corresponding octet string of length xLen
+    X -- corresponding octet string of length xLen, a bytearray
     """
     if x >= pow(256, x_len):
         return "integer too large"
+    print("i2osp x: {0}".format(x))
+    x_array = bytearray(x.to_bytes(x_len, byteorder="big", signed="False"))
+    print("i2osp x_array: {0}".format(x_array))
 
-    x_new = 0
-    for i in range(256):
-        x_new += ( ( x >> i ) | 0 ) * pow(256, i)
-
-    X = 0
-    for i in range(1,x_len+1):
-        X += ( ( x_new >> ( x_len - i ) ) | 0 ) << ( x_len - i )
+    x_array.reverse()
+    X = x_array.copy()
+    # X = bytearray(int(0).to_bytes(x_len, byteorder="big", signed="False"))
+    # for i in range(x_len-1):
+    #     print("i2osp x_array[{1}]: {0}".format(x_array[x_len - i], x_len - i))
+    #     print("i2osp X[{1}]: {0}".format(X[i], i))
+    #     X[i] += x_array[x_len - i]
 
     return X
 
@@ -39,26 +40,25 @@ def mgf1(mgf_seed, mask_len):
     mgf_seed -- seed from which mask is generated, an octet string
     mask_len -- intended length in octets of the mask, at most 2^32 hLen
     """
-    mask = 0
-    if mask_len > pow(2, 32):
+    h_len = 20 # length of hash output in octets
+
+    if mask_len > (pow(2, 32) * h_len):
         return "mask too long"
-    T = 0
+    T = bytearray(b'')
 
-    for counter in range(math.ceil(mask_len / h_len - 1)):
-        C = i2osp(counter, 4)
-        # T = T + hash(mgf_seed + C)            
-        h = hashlib.sha1(bytearray(mgf_seed) + bytearray(C)) # may have to convert to oct()
-        dig = h.digest()
-        # print(type(dig))
-        T +=  int.from_bytes(dig, byteorder="big")
+    for counter in range(math.ceil(mask_len / h_len) - 1):
+        C = i2osp(counter, 4) # returns a bytearray
+        print("C is: {0}".format(C))
+        h = hashlib.sha1(bytearray(mgf_seed) + C)
+        dig = bytearray(h.digest())
+        T += dig
+        print("T is: {0}".format(T))
 
-    output = 0
-    for i in range(mask_len):
-        output += 0
-
-    return hex(T >> ( len(T) - mask_len ))
+    return T[:mask_len]
 
 if __name__ == '__main__':
+    print("return from i2osp: {0}\n".format(i2osp(int("11111111111111111111111111111111", 2), 8)))
+
     parser = argparse.ArgumentParser(description="Assignment B3")
 
     parser.add_argument("--mgfseed", help="mgfSeed", type=str)
@@ -66,10 +66,8 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    print(hex(int(args.mgfseed, 16)))
-    mgf_seed = binascii.unhexlify(args.mgfseed)
-    print(mgf_seed)
-    print(type(mgf_seed))
-    print(len(mgf_seed))
+    print(args)
 
-    print(mgf1(mgf_seed, args.masklen))
+    mgf_seed = binascii.unhexlify(args.mgfseed)
+
+    print("final output: {0}".format(binascii.hexlify(mgf1(mgf_seed, args.masklen))))
